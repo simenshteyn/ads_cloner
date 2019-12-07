251587bcc3752aa259a5188a1efea16b87770684
Sergey Simenshteyn <simenshteyn@gmail.com>
2019-12-07 10:03:55 +0700

    Working mock login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:flutter_vk_sdk/flutter_vk_sdk.dart';
+import 'package:flutter_vk_sdk/vk_scope.dart';
 
 class LoginPage extends StatelessWidget {
+  void vkLogin() async {
+    FlutterVKSdk.login(
+        scope: '${VKScope.ads}, ${VKScope.offline}',
+        onSuccess: (res) {
+          print('login is ok');
+        },
+        onError: (error) {
+          print('LOGIN ERROR: $error}');
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +28,7 @@ class LoginPage extends StatelessWidget {
             FlatButton(
               color: Colors.blue,
               textColor: Colors.white,
-              onPressed: () {},
+              onPressed: vkLogin,
               child: Text(
                 'Login VK',
                 style: TextStyle(fontSize: 20.0),
