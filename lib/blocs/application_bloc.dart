acf471fa9ebf4f98b20bb275cf63e1e3710a997d
Sergey Simenshteyn <simenshteyn@gmail.com>
2019-12-13 20:36:10 +0700

    working login and account list

--- lib/blocs/application_bloc.dart
+++ lib/blocs/application_bloc.dart
@@ -1,34 +1,52 @@
 import 'dart:async';
 import 'package:ads_cloner/blocs/bloc_provider.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_vk_sdk/models/vk_access_token.dart';
+import 'package:ads_cloner/models/accounts_list.dart';
 
 class ApplicationBloc implements BlocBase {
-  String _apiKey;
-  String _userId;
+  VKAccessToken vkAccessToken;
+  AccountsList accountsList;
 
-  StreamController<String> _apiKeyController = StreamController<String>();
-  StreamSink<String> get _inApiKey => _apiKeyController.sink;
-  Stream<String> get outApiKey => _apiKeyController.stream;
+  StreamController<VKAccessToken> _tokenController =
+      StreamController<VKAccessToken>.broadcast();
+  StreamSink<VKAccessToken> get inVkAccessToken => _tokenController.sink;
+  Stream<VKAccessToken> get outVkAccessToken => _tokenController.stream;
 
-  StreamController<String> _userIdController = StreamController<String>();
-  StreamSink<String> get _inUserId => _userIdController.sink;
-  Stream<String> get outUserId => _userIdController.stream;
+  StreamController _cmdTokenController = StreamController();
+  StreamSink get getVkAccessToken => _cmdTokenController.sink;
+
+  StreamController<AccountsList> _accountsController =
+      StreamController<AccountsList>.broadcast();
+  StreamSink<AccountsList> get inAccountsList => _accountsController.sink;
+  Stream<AccountsList> get outAccountsList => _accountsController.stream;
+
+  StreamController _cmdAccountsController = StreamController();
+  StreamSink get getAccountsList => _cmdAccountsController.sink;
 
   ApplicationBloc() {
-    _apiKeyController.stream.listen(_handleLogicApiKeyController);
-    _userIdController.stream.listen(_handleLogicUserIdController);
+    _tokenController.stream.listen(_handleLogicTokenController);
+    _cmdTokenController.stream.listen((token) {
+      _tokenController.sink.add(token);
+    });
+
+    _accountsController.stream.listen(_handleAccountsController);
+    _cmdAccountsController.stream.listen((accounts) {
+      _accountsController.sink.add(accounts);
+    });
   }
 
   void dispose() {
-    _apiKeyController.close();
-    _userIdController.close();
+    _tokenController.close();
+    _cmdTokenController.close();
+    _accountsController.close();
+    _cmdAccountsController.close();
   }
 
-  void _handleLogicApiKeyController(data) {
-    //handle some data
+  void _handleLogicTokenController(data) {
+    vkAccessToken = data;
   }
 
-  void _handleLogicUserIdController(data) {
-    //handle some data
+  void _handleAccountsController(data) {
+    accountsList = data;
   }
 }
---
+++ test/accounts_list_test.dart
@@ -0,0 +1,14 @@
+import 'package:test/test.dart';
+import 'package:ads_cloner/models/accounts_list.dart';
+import 'dart:async';
+
+void main(){
+  test('AccountsList Class testing from JSON',
+  () async {
+    final stringJson = '{"response":[{"account_id":1600058392,"account_type":"general","account_status":1,"account_name":"Личный кабинет","access_role":"admin"},{"account_id":1604230960,"account_type":"general","account_status":1,"account_name":"Наталья Колотилина","access_role":"manager"},{"account_id":1604576992,"account_type":"general","account_status":1,"account_name":"Евгений Терехов","access_role":"manager"},{"account_id":1604847002,"account_type":"general","account_status":1,"account_name":"Алексей Ветров","access_role":"reports"},{"account_id":1605240452,"account_type":"general","account_status":1,"account_name":"Алексей Куколь","access_role":"manager"}]}';
+    final accs = AccountsList.fromJSON(stringJson);
+    expect(accs.accounts.length, 5);
+    expect(accs.accounts[0].accountName, 'Личный кабинет');
+  });
+}
+
