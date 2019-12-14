17d3a687a0642f3fbeb89b351e6a503d3330c0eb
Sergey Simenshteyn <simenshteyn@gmail.com>
2019-12-14 12:50:39 +0700

    application bloc currentAccountController added

--- lib/blocs/application_bloc.dart
+++ lib/blocs/application_bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:ads_cloner/blocs/bloc_provider.dart';
+import 'package:ads_cloner/models/account.dart';
 import 'package:flutter_vk_sdk/models/vk_access_token.dart';
 import 'package:ads_cloner/models/accounts_list.dart';
 import 'package:ads_cloner/models/campaigns_list.dart';
@@ -9,6 +10,7 @@ class ApplicationBloc implements BlocBase {
   VKAccessToken vkAccessToken;
   AccountsList accountsList;
   CampaignsList campaignsList;
+  Account currentAccount;
 
   StreamController<VKAccessToken> _tokenController =
       StreamController<VKAccessToken>.broadcast();
@@ -33,6 +35,13 @@ class ApplicationBloc implements BlocBase {
   StreamController _cmdCampaignsController = StreamController();
   StreamSink get getCampaignsList => _cmdCampaignsController.sink;
 
+  StreamController<Account> _currentAccountConroller = StreamController<Account>.broadcast();
+  StreamSink<Account> get inCurrentAccount => _currentAccountConroller.sink;
+  Stream<Account> get outCurrentAccount => _currentAccountConroller.stream;
+
+  StreamController _cmdCurrentAccountController = StreamController();
+  StreamSink get getCurrentAccount => _cmdCurrentAccountController.sink;
+
   ApplicationBloc() {
     _tokenController.stream.listen(_handleLogicTokenController);
     _cmdTokenController.stream.listen((token) {
@@ -48,6 +57,12 @@ class ApplicationBloc implements BlocBase {
     _cmdCampaignsController.stream.listen((campaigns) {
       _campaignsController.sink.add(campaigns);
     });
+
+    _currentAccountConroller.stream.listen(_handleCurrentAccountController);
+    _cmdCurrentAccountController.stream.listen((account) {
+      _currentAccountConroller.sink.add(account);
+    });
+
   }
 
   void dispose() {
@@ -57,6 +72,8 @@ class ApplicationBloc implements BlocBase {
     _cmdAccountsController.close();
     _campaignsController.close();
     _cmdCampaignsController.close();
+    _currentAccountConroller.close();
+    _cmdCurrentAccountController.close();
   }
 
   void _handleLogicTokenController(data) {
@@ -69,5 +86,9 @@ class ApplicationBloc implements BlocBase {
 
   void _handleCampaignsController(data) {
     campaignsList = data;
+  }
+
+  void _handleCurrentAccountController(data) {
+    currentAccount = data;
   }
 }
--- lib/pages/campaigns_page.dart
+++ lib/pages/campaigns_page.dart
@@ -1,3 +1,5 @@
+import 'package:ads_cloner/blocs/campaigns_bloc.dart';
+import 'package:ads_cloner/models/campaigns_request.dart';
 import 'package:flutter/material.dart';
 import 'package:ads_cloner/blocs/bloc_provider.dart';
 import 'package:ads_cloner/blocs/application_bloc.dart';
@@ -7,7 +9,38 @@ class CampaignsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ApplicationBloc appBloc = BlocProvider.of<ApplicationBloc>(context);
+    CampaignsBloc bloc = BlocProvider.of<CampaignsBloc>(context);
+    bloc.getCampaignsList
+        .add(CampaignsRequest(appBloc.vkAccessToken, appBloc.currentAccount));
 
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Choose ads campaign'),
+      ),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: StreamBuilder<CampaignsList>(
+            //initialData: loginBloc.accounts,
+            stream: bloc.outCampaignsList,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  itemCount: snapshot.data.campaigns.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      leading: Icon(Icons.account_circle),
+                      title: Text(snapshot.data.campaigns[index].name),
+                      trailing: Icon(Icons.keyboard_arrow_right),
+                      onTap: () {
+                        print('Campaign tapped index is ${index}');
+                      },
+                    );
+                  },
+                );
+              }
+              return Center(child: CircularProgressIndicator());
+            }),
+      ),
+    );
   }
 }
