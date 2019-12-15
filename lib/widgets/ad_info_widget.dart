3ed4082f8d31a390bc972781e152e983c03c1741
Sergey Simenshteyn <simenshteyn@gmail.com>
2019-12-15 22:46:53 +0700

    working ad preview

--- lib/widgets/ad_info_widget.dart
+++ lib/widgets/ad_info_widget.dart
@@ -1,5 +1,6 @@
 import 'package:ads_cloner/blocs/application_bloc.dart';
 import 'package:ads_cloner/blocs/bloc_provider.dart';
+import 'package:ads_cloner/models/ad.dart';
 import 'package:flutter/material.dart';
 
 class AdInfoWidget extends StatelessWidget {
@@ -12,51 +13,56 @@ class AdInfoWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     ApplicationBloc appBloc = BlocProvider.of<ApplicationBloc>(context);
     return SingleChildScrollView(
-      child: ExpansionTile(
-        title: Text(
-          'Название объявления',
-          style: _textStyleUp,
-          textAlign: TextAlign.center,
-        ),
-        subtitle: Text(
-          appBloc.currentAd.name,
-          style: _textStyleDown,
-          textAlign: TextAlign.center,
-        ),
-        children: <Widget>[
-          Divider(),
-          ListTile(
-            title: Text('Название кампании',
-                style: _textStyleUp),
-            subtitle: Text(appBloc.currentCampaign.name,
-                style: _textStyleDown),
-          ),
-          ListTile(
-            title: Text('Дневной лимит',
-                style: _textStyleUp),
-            subtitle: Text(
-                appBloc.currentAd.dayLimit == '0'
-                    ? "Не задан"
-                    : appBloc.currentAd.dayLimit,
-                style: _textStyleDown),
-          ),
-          ListTile(
-            title: Text('Лимит объявления',
-                style: _textStyleUp),
-            subtitle: Text(
-                appBloc.currentAd.allLimit == '0'
-                    ? "Не задан"
-                    : '${appBloc.currentAd.allLimit} ₽',
-                style: _textStyleDown),
-          ),
-          ListTile(
-            title: Text('Цена за 1000 показов',
-                style: _textStyleUp),
-            subtitle: Text(appBloc.currentAd.cpm,
-                style: _textStyleDown),
-          ),
-        ],
-      ),
+      child: StreamBuilder<Ad>(
+          stream: appBloc.outCurrentAd,
+          initialData: appBloc.currentAd,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ExpansionTile(
+                title: Text(
+                  'Название объявления',
+                  style: _textStyleUp,
+                  textAlign: TextAlign.center,
+                ),
+                subtitle: Text(
+                  snapshot.data.name,
+                  style: _textStyleDown,
+                  textAlign: TextAlign.center,
+                ),
+                children: <Widget>[
+                  Divider(),
+                  ListTile(
+                    title: Text('Название кампании', style: _textStyleUp),
+                    subtitle: Text(appBloc.currentCampaign.name,
+                        style: _textStyleDown),
+                  ),
+                  ListTile(
+                    title: Text('Дневной лимит', style: _textStyleUp),
+                    subtitle: Text(
+                        snapshot.data.dayLimit == '0'
+                            ? "Не задан"
+                            : snapshot.data.dayLimit,
+                        style: _textStyleDown),
+                  ),
+                  ListTile(
+                    title: Text('Лимит объявления', style: _textStyleUp),
+                    subtitle: Text(
+                        snapshot.data.allLimit == '0'
+                            ? "Не задан"
+                            : '${appBloc.currentAd.allLimit} ₽',
+                        style: _textStyleDown),
+                  ),
+                  ListTile(
+                    title: Text('Цена за 1000 показов', style: _textStyleUp),
+                    subtitle: Text(
+                        '${snapshot.data.cpm.substring(0, snapshot.data.cpm.length - 2)}.${snapshot.data.cpm.substring(snapshot.data.cpm.length - 2)} ₽',
+                        style: _textStyleDown),
+                  ),
+                ],
+              );
+            }
+            return Center(child: CircularProgressIndicator());
+          }),
     );
   }
 }
