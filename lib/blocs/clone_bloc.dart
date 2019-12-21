e5733ac7a61ed9eba4be8b2330317ff5fdd182e8
Sergey Simenshteyn <simenshteyn@gmail.com>
2019-12-21 16:55:10 +0700

    working clone page draft

--- lib/blocs/clone_bloc.dart
+++ lib/blocs/clone_bloc.dart
@@ -2,25 +2,33 @@ import 'dart:async';
 
 import 'package:ads_cloner/api/vk_api.dart';
 import 'package:ads_cloner/blocs/bloc_provider.dart';
+import 'package:ads_cloner/models/ads_targeting_list.dart';
+import 'package:ads_cloner/models/ads_targeting_request.dart';
 import 'package:ads_cloner/models/wall_post_list.dart';
 import 'package:ads_cloner/models/wall_post_request.dart';
 
 class CloneBloc implements BlocBase {
   WallPostList _wallPostList;
+  AdsTargetingList _adsTargetingList;
 
   StreamController<WallPostList> _wallPostController =
       StreamController<WallPostList>.broadcast();
   Stream<WallPostList> get outWallPostList => _wallPostController.stream;
-
   StreamController<WallPostRequest> _cmdWallPostController =
       StreamController<WallPostRequest>.broadcast();
   StreamSink<WallPostRequest> get getWallPostList =>
       _cmdWallPostController.sink;
 
+  StreamController<AdsTargetingList> _adsTargetingController = StreamController<AdsTargetingList>.broadcast();
+  Stream<AdsTargetingList> get outAdsTargetingList => _adsTargetingController.stream;
+  StreamController<AdsTargetingRequest> _cmdAdsTargetingController = StreamController<AdsTargetingRequest>.broadcast();
+  StreamSink<AdsTargetingRequest> get getAdsTargetingList => _cmdAdsTargetingController.sink;
+
   CloneBloc() {
     print("CLONE BLOC CREATED");
 
     _wallPostController.stream.listen(_handleLogic);
+    _adsTargetingController.stream.listen(_handleTargetingLogic);
 
     _cmdWallPostController.stream.listen((WallPostRequest req) {
       var vk = VkApi(userToken: req.vkAccessToken.token);
@@ -29,15 +37,29 @@ class CloneBloc implements BlocBase {
         _wallPostController.sink.add(_wallPostList);
       });
     });
+
+    _cmdAdsTargetingController.stream.listen((AdsTargetingRequest req) {
+      var vk = VkApi(userToken: req.vkAccessToken.token);
+      vk.adsGetAdsTargeting(req.account.accountId.toString(), req.ad).then((list) {
+        _adsTargetingList = list;
+        _adsTargetingController.sink.add(_adsTargetingList);
+      });
+    });
   }
 
   void dispose() {
     _wallPostController.close();
     _cmdWallPostController.close();
+    _adsTargetingController.close();
+    _cmdAdsTargetingController.close();
     print("CLONE BLOC DISPOSED");
   }
 
   void _handleLogic(data) {
     _wallPostList = data;
+  }
+
+  void _handleTargetingLogic(data) {
+    _adsTargetingList = data;
   }
 }
--- lib/pages/clone_page.dart
+++ lib/pages/clone_page.dart
@@ -1,6 +1,8 @@
 import 'package:ads_cloner/blocs/application_bloc.dart';
 import 'package:ads_cloner/blocs/bloc_provider.dart';
 import 'package:ads_cloner/blocs/clone_bloc.dart';
+import 'package:ads_cloner/models/ads_targeting_list.dart';
+import 'package:ads_cloner/models/ads_targeting_request.dart';
 import 'package:ads_cloner/models/wall_post_list.dart';
 import 'package:ads_cloner/models/wall_post_request.dart';
 import 'package:flutter/material.dart';
@@ -12,19 +14,33 @@ class ClonePage extends StatelessWidget {
     CloneBloc bloc = BlocProvider.of<CloneBloc>(context);
     bloc.getWallPostList
         .add(WallPostRequest(appBloc.vkAccessToken, appBloc.currentPostId));
+    bloc.getAdsTargetingList.add(AdsTargetingRequest(
+        appBloc.vkAccessToken, appBloc.currentAccount, appBloc.currentAd));
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Clone Ads'),
       ),
-      body: StreamBuilder<WallPostList>(
-        stream: bloc.outWallPostList,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-          return Text('Some testing ${snapshot.data.wallPosts[0].ownerId}');
-          }
-          return CircularProgressIndicator();
-        }
+      body: Column(
+        children: <Widget>[
+          StreamBuilder<WallPostList>(
+              stream: bloc.outWallPostList,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text('Some testing ${snapshot.data.wallPosts[0].ownerId}');
+                }
+                return CircularProgressIndicator();
+              }),
+          StreamBuilder<AdsTargetingList>(
+            stream: bloc.outAdsTargetingList,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text('Some targeting info ${snapshot.data.adsTargeting[0].campaignId}');
+              }
+              return CircularProgressIndicator();
+            }
+          )
+        ],
       ),
     );
   }
