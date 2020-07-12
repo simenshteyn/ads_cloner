d7c961501d7102e87a793ded7fbb6e8a7451f3df
Sergey Simenshteyn <simenshteyn@gmail.com>
2020-07-12 18:03:20 +0700

    working bage

--- lib/pages/clone_text_page.dart
+++ lib/pages/clone_text_page.dart
@@ -2,6 +2,7 @@ import 'package:ads_cloner/blocs/application_bloc.dart';
 import 'package:ads_cloner/blocs/bloc_provider.dart';
 import 'package:ads_cloner/blocs/clone_text_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart';
 
 class CloneTextPage extends StatefulWidget {
   @override
@@ -28,64 +29,78 @@ class _CloneTextPageState extends State<CloneTextPage> {
   Widget build(BuildContext context) {
     CloneTextBloc bloc = BlocProvider.of<CloneTextBloc>(context);
     final bool showFab = MediaQuery.of(context).viewInsets.bottom == 0.0;
+    //final bool showFab = true;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Enter text'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: TextField(
-              keyboardType: TextInputType.multiline,
-              maxLines: null,
-              controller: _textController,
-              autofocus: true,
-              decoration: InputDecoration(
-                hintText: 'Введите текст',
-                suffix: IconButton(
-                  icon: Icon(Icons.add),
-                  onPressed: () {
-                    if (_textField != "") {
-                      bloc.addTextToList.add(_textField);
-                      _textController.clear();
-                      FocusScope.of(context).unfocus();
-                    }
-                  },
+        appBar: AppBar(
+          title: Text('Варианты текстов'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                controller: _textController,
+                autofocus: true,
+                decoration: InputDecoration(
+                  hintText: 'Введите текст',
+                  suffix: IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      if (_textField != "") {
+                        bloc.addTextToList.add(_textField);
+                        _textController.clear();
+                        //FocusScope.of(context).unfocus();
+                      }
+                    },
+                  ),
                 ),
               ),
             ),
-          ),
-          Flexible(
-            child: StreamBuilder<List<String>>(
-                stream: bloc.outTextList,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return ListView.separated(
-                      separatorBuilder: (BuildContext context, int index) =>
-                          Divider(),
-                      itemCount: snapshot.data.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return ListTile(
-                          title: Text(snapshot.data[index]),
-                        );
-                      },
+            Flexible(
+              child: StreamBuilder<List<String>>(
+                  stream: bloc.outTextList,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ListView.separated(
+                        separatorBuilder: (BuildContext context, int index) =>
+                            Divider(),
+                        itemCount: snapshot.data.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return ListTile(
+                            title: Text(snapshot.data[index]),
+                          );
+                        },
+                      );
+                    }
+                    return Center(
+                      child: Text('Вариантов пока нет…'),
                     );
-                  }
-                  return Center(
-                    child: Text('Пока текстов нет…'),
-                  );
-                }),
-          ),
-        ],
-      ),
-      floatingActionButton: showFab
-          ? FloatingActionButton(
-              child: Icon(Icons.add),
-              onPressed: () {},
-            )
-          : null,
-    );
+                  }),
+            ),
+          ],
+        ),
+        floatingActionButton: StreamBuilder<List<String>>(
+            stream: bloc.outTextList,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                int badgeText = snapshot.data.length;
+                return Badge(
+                  animationType: BadgeAnimationType.scale,
+                  badgeContent: Text(badgeText.toString(),
+                      style: TextStyle(color: Colors.white)),
+                  position: BadgePosition.topRight(top: -3, right: -3),
+                  child: FloatingActionButton(
+                    child: Icon(Icons.send),
+                    onPressed: () {},
+                  ),
+                );
+              }
+              print('NO DATA');
+              return Container();
+            }));
   }
 
   void _saveLastValue() {
