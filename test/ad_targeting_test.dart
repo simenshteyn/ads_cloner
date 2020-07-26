99088b807cf868c79b854b5111674f3a52126a45
Sergey Simenshteyn <simenshteyn@gmail.com>
2020-07-26 14:32:53 +0700

    new test

---
+++ test/ad_targeting_test.dart
@@ -0,0 +1,21 @@
+import 'package:ads_cloner/models/ad_targeting.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('create ads targeting from JSON', () async {
+    final map = {
+      "campaign_id": "1014488300",
+      "id": "67578692",
+      "sex": "1",
+      "country": "1",
+      "cities": "99",
+      "groups_formula": "!25212245",
+      "retargeting_groups": "32228380",
+      "retargeting_groups_not": "29569053,32233473,32228458",
+      "count": "2100"
+    };
+    var targeting = AdTargeting.fromJSON(map);
+    print(targeting.toJson());
+    expect(targeting.sex, "1");
+  });
+}
